7d442fa3d1d2f7289d1f698813be6f870c03bbb5
jorgepinazero <jorge.pina@polimentes.mx>
2018-11-06 10:40:59 -0600

    jorge-slider and service working

--- src/app/instagram.service.ts
+++ src/app/instagram.service.ts
@@ -1,8 +1,25 @@
-import {Observable} from 'rxjs';
-import {Input} from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Http, Response} from '@angular/http';
+import {Injectable} from '@angular/core';
 
-export class InstagramService{
-@Input() url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=4536554789.0dc7b40.fb494b1d649f4b48ab78b0afcf5bdaa7';
+@Injectable()
+export class InstagramService {
+  private apiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=4536554789.0dc7b40.fb494b1d649f4b48ab78b0afcf5bdaa7';
+  data: any = {};
 
+  constructor(public http: Http) {
+    this.getData();
+    this.importData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+      .pipe(map ((res: Response) => res.json()));
+  }
+
+  importData() {
+    this.getData().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
